14c8e6f46549dd4637ce4c5cf1b2e24b585582d4
David Moody <david@davidxmoody.com>
2017-10-04 10:33:03 +0100

    Format success screen a bit better

--- src/components/screens/SuccessScreen.js
+++ src/components/screens/SuccessScreen.js
@@ -5,8 +5,10 @@ export default function WelcomeScreen(props) {
   return (
     <Screen title="Thank you!">
       <p>
-        Your data has been submitted to the server.
-        Reload the app if you would like to try it again.
+        Your data has been submitted.
+      </p>
+      <p>
+        Please reload the app if you would like to try it again.
       </p>
     </Screen>
   )
