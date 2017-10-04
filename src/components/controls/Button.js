108cb0aef2241536506cf591c20f3f93062a575f
David Moody <david@davidxmoody.com>
2017-10-04 09:33:47 +0100

    Disable button when busy

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -5,6 +5,7 @@ import "./Button.css"
 export default function Button(props) {
   return (
     <button
+      disabled={props.busy}
       className={`button ${props.busy ? "button--busy" : ""}`}
       onClick={props.onClick}
     >{props.children}</button>
